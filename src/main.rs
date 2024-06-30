5ad3236c7e938649c7686997bc8bdc06ee5c743d
Kieran Llarena <baymax709@gmail.com>
2024-06-30 16:27:34 -0400

    feat: print user options infinitely

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,17 @@
 use std::io;
 
 fn main() {
-    println!("Hello, world!");
+    loop {
+        print_all_options();
+    }
+}
+
+fn print_all_options() {
+    println!("What would you like to do?");
+    println!("1. Create a todo");
+    println!("2. Read a todo");
+    println!("3. Update a todo");
+    println!("4. Delete a todo");
+    println!("5. List all todos");
+    println!("6. Exit");
 }
